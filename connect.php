98e2da963ad28725fcac83bc42304927de39b4fd
Shaun Henderson <kurdtpage@gmail.com>
2024-11-15 23:07:15 +1300

    Added documentation to top of file

--- connect.php
+++ connect.php
@@ -1,5 +1,63 @@
 <?php
 
+/*
+## Select
+```
+╔══════════╤═════════════════════════════════════════╤══════════════════════════════════════════════════╗
+║          │ Placeholders no (query)                 │ Placeholders yes (prepare, execute)              ║
+╟──────────┼─────────────────────────────────────────┼──────────────────────────────────────────────────╢
+║ Single   │                                         │ $params = ['id' => $id];                         ║
+║ row      │ $a_json['sql'] = $pdo->niceQuery($sql); │ $a_json['sql'] = $pdo->niceQuery($sql, $params); ║
+║          │ $stmt = $pdo->run($sql);                │ $stmt = $pdo->run($sql, $params);                ║
+║          │ if ($table = $stmt->fetch()) {          │ if ($table = $stmt->fetch()) {                   ║
+║          │     $a_json['id'] = $table['id'];       │     $a_json['id'] = $table['id'];                ║
+║          │ } else {                                │ } else {                                         ║
+║          │     //no data                           │     //no data                                    ║
+║          │ }                                       │ }                                                ║
+╟──────────┼─────────────────────────────────────────┼──────────────────────────────────────────────────╢
+║ Multiple │                                         │ $params = ['id' => $id];                         ║
+║ rows     │ $a_json['sql'] = $pdo->niceQuery($sql); │ $a_json['sql'] = $pdo->niceQuery($sql, $params); ║
+║          │ $stmt = $pdo->run($sql);                │ $stmt = $pdo->run($sql, $params);                ║
+║          │ while ($table = $stmt->fetch()) {       │ while ($table = $stmt->fetch()) {                ║
+║          │     $a_json['id'] = $table['id'];       │     $a_json['id'] = $table['id'];                ║
+║          │ }                                       │ }                                                ║
+╚══════════╧═════════════════════════════════════════╧══════════════════════════════════════════════════╝
+```
+
+## Insert
+```
+$sql = 'INSERT INTO users (
+	first, last
+) VALUES (
+	:first, :last
+)';
+$params = [
+	'first' => $first,
+	'last' => $last,
+];
+$a_json['sql'] = $pdo->niceQuery($sql, $params);
+$pdo->run($sql, $params);
+$id = $pdo->lastInsertId();
+```
+
+## Update
+```
+$sql = 'UPDATE users SET
+	first = :first,
+	last = :last
+WHERE
+	id = :id';
+$params = [
+	'first' => $first,
+	'last' => $last,
+	'id' => $id,
+];
+$a_json['sql'] = $pdo->niceQuery($sql, $params);
+$pdo->run($sql, $params);
+$affected_rows = $stmt->rowCount();
+```
+*/
+
 class MyPDO extends PDO {
 	public function run($sql, $data = [])
 	{
